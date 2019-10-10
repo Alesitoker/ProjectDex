c06caebec0752937be3fc7c11ed23d909f04d098
Alejandro <alesitonightray@gmail.com>
2019-10-10 11:12:07 +0200

    Initial commit

---
+++ app/src/main/java/es/saladillo/alejandrodiaz/projectdex/ui/main/MainActivity.java
@@ -0,0 +1,16 @@
+package es.saladillo.alejandrodiaz.projectdex.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.saladillo.alejandrodiaz.projectdex.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
